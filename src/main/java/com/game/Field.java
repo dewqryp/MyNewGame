4b89ac957a75356be7cc6d9e67be8bf28b95b344
Ярослав Ярошевский <pomidorkaniga228@gmail.com>
2026-09-05 21:41:52 +0300

    added Field class

---
+++ src/main/java/com/game/Field.java
@@ -0,0 +1,52 @@
+package com.game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field extends JPanel {
+    public static final float GRAVITY = 0.5f;
+
+    private final List<Apple> apples = new ArrayList<>();
+
+    private final Physicist player;
+    private final Tree tree;
+
+    public Field() {
+
+        player = new Physicist(100, 100);
+        tree = new Tree(400, 300);
+
+        setPreferredSize(new Dimension(800, 500));
+    }
+
+    public void tossApple() {
+
+        Apple apple = player.takeApple();
+
+        apple.toss(
+                player.getAimingAngle(),
+                player.getAimingForce()
+        );
+
+        apples.add(apple);
+    }
+
+    public void updateGame() {
+
+        for (Apple apple : apples) {
+            apple.step();
+        }
+
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        // Здесь будем рисовать игру
+    }
+}
