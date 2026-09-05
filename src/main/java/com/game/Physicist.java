df2d9055407e4b1bc3de8014d5d44a7f0f66e9d8
Ярослав Ярошевский <pomidorkaniga228@gmail.com>
2026-09-05 21:40:49 +0300

    added Physicist class

---
+++ src/main/java/com/game/Physicist.java
@@ -0,0 +1,50 @@
+package com.game;
+
+public class Physicist implements GamePiece {
+    private int x;
+    private int y;
+
+    private float aimingAngle;
+    private float aimingForce;
+
+    public Physicist(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getAimingAngle() {
+        return aimingAngle;
+    }
+
+    public void setAimingAngle(float aimingAngle) {
+        this.aimingAngle = aimingAngle;
+    }
+
+    public float getAimingForce() {
+        return aimingForce;
+    }
+
+    public void setAimingForce(float aimingForce) {
+        this.aimingForce = aimingForce;
+    }
+
+    public Apple takeApple() {
+        return new Apple(x, y);
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
